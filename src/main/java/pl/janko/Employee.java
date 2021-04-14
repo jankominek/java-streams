51d2b39c63dafa595b167cfe25e0a8a4851639a9
jankominek <janekkominek2000@gmail.com>
2021-04-14 22:49:24 +0200

    test

---
+++ src/main/java/pl/janko/Employee.java
@@ -0,0 +1,15 @@
+package pl.janko;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Employee(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+}
---
+++ src/main/java/pl/janko/Main.java
@@ -0,0 +1,15 @@
+package pl.janko;
+
+public class Main {
+
+    public Main(){
+
+    }
+    private static Employee[] arrayOfEmps = {
+            new Employee(1, "Jeff Bezos", 55),
+            new Employee(2, "Bill Gates", 34),
+            new Employee(3, "Mark Zuckerberg", 18)
+    };
+
+
+}
