312d458f34811fd63d13650ebf23e0c80ff03d10
jankominek <janekkominek2000@gmail.com>
2021-05-05 02:15:27 +0200

    changes in ModelExercise.java (added gson save to file)

--- src/main/java/pl/janko/exercises/exercise1/Exercise1Result.java
+++ src/main/java/pl/janko/exercises/exercise1/Exercise1Result.java
@@ -2,23 +2,36 @@ package pl.janko.exercises.exercise1;
 
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Exercise1Result {
+public class Exercise1Result{
 
-    List<Country> countries = new ArrayList<>();
+    ModelExercise modelExercise;
     List<Temp> tempList = new ArrayList<>();
 
     public Exercise1Result() throws FileNotFoundException {
-            ModelExercise modelExercise = new ModelExercise();
+            this.modelExercise = new ModelExercise();
             //countries = modelExercise.getCountries();
     }
 
     public void res1(){
+
+        Map<String, List<Integer>> m = new HashMap<>();
+        m.put("a", Arrays.asList(1, 2, 3));
+        m.put("b", Arrays.asList(1, 2, 3));
+        m.put("c", Arrays.asList(1, 2, 3));
+        m.put("d", Arrays.asList(1, 2, 3));
+        m.put("e", Arrays.asList(1, 2, 3));
         //      Topic: Find the highest populated country of each continent:
         System.out.println("\nres1 -------------------------- \n");
         Map<String, Integer> res1 = new HashMap<>();
-        tempList = this.getTemplist();
+//        res1 = m.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.collectingAndThen(
+//                Collectors.reducing(BinaryOperator.maxBy(Integer:))));
+        //res1 = m.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Comparator.comparing(Map.Entry::getValue)));
+
 
     }
 
@@ -33,5 +46,7 @@ public class Exercise1Result {
             );
             return list;
     }
+
+
 
 }
--- src/main/java/pl/janko/exercises/exercise1/ModelExercise.java
+++ src/main/java/pl/janko/exercises/exercise1/ModelExercise.java
@@ -1,15 +1,13 @@
 package pl.janko.exercises.exercise1;
 
+import com.google.gson.Gson;
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -17,9 +15,11 @@ import java.util.stream.Stream;
 
 public class ModelExercise {
 
-    Map<String, List<Country>> continentsMap = new TreeMap<>();
+    public Map<String, List<Country>> continentsMap;
     String fileName = "C:\\Users\\Janek\\Desktop\\java-streams\\worldometer_data.csv";
     List<Continent> continents = new ArrayList<>();
+    Gson gson = new Gson();
+    String pathToData = "src/main/java/data/dataMap.json";
 
     public ModelExercise() throws FileNotFoundException {
 
@@ -31,7 +31,7 @@ public class ModelExercise {
 
         System.out.println("countries size -> " + countries.size());
 
-        // creating TreeMap ("continentName", Country) by grouping by
+        // creating Map ("continentName", Country) by grouping by
         continentsMap = countries.stream().collect(Collectors.groupingBy(f->f.getContinent()));
 
         //searching element with specific key and display it by stream
@@ -42,7 +42,36 @@ public class ModelExercise {
                 m.getValue().stream().forEach( obj -> System.out.println(obj.getName()));
             }
         }
+        //this.saveMainMaptoJson();
+
     }
 
+    public Map<String, List<Country>> getContinentsMap() {
+        return continentsMap;
+    }
 
+    private void saveMainMaptoJson(){
+
+        String jsonFile = gson.toJson(continentsMap);
+        try(FileWriter fileWriter = new FileWriter(pathToData)){
+            fileWriter.write(jsonFile);
+            fileWriter.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public <K, V> void removeNulls(Map<K, V> map)
+    {
+        System.out.println("map before ----> " + map.size());
+        Iterator<Map.Entry<K, V>> itr = map.entrySet().iterator();
+        while (itr.hasNext())
+        {
+            Map.Entry<K, V> curr = itr.next();
+            if (curr.getValue() == null) {
+                itr.remove();
+            }
+        }
+        System.out.println("map after ----> " + map.size());
+    }
 }
